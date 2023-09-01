ea72a728fda3e6bf33be2532aefe6eed77ac7115
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-01 20:18:10 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 read -p "[$] Masukkan nama file yang ingin dienkripsi: " nfa
-nfe="${nfa}.RF"
-if [ ! -f "$nfa" ]; then
+if [ -f "$nfa" ]; then
+  nfe="${nfa}.RF"
+  base64 "$nfa" > "$nfe"
+  echo "[+] File $nfa telah dienkripsi."
+  exit 0
+else
   echo "[!] File $nfa tidak ditemukan."
   exit 1
 fi
-base64 "$nfa" > "$nfe"
-echo "[+] File $nfa telah dienkripsi."

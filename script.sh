832b75c22af37ba8eab91dd71726d0db98cc47c7
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-01 20:22:35 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+keluar(){
+  echo ""
+  read -p "Tekan Enter untuk keluar."
+  echo ""
+}
+
 read -p "[$] Masukkan nama file yang ingin dienkripsi: " nfa
 if [ -f "$nfa" ]; then
   nfe="${nfa}.RF"
   base64 "$nfa" > "$nfe"
   echo "[+] File $nfa telah dienkripsi."
+  keluar
   exit 0
 else
   echo "[!] File $nfa tidak ditemukan."
+  keluar
   exit 1
 fi
